5c8b46123ea9a60e3abd6246c48902cd8fc7bd7c
Gunashree S <gunashrees341@gmail.com>
2021-07-29 18:44:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ var cars, car1, car2, car3, car4;
 
 
 function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  canvas = createCanvas(500,500);
   database = firebase.database();
   game = new Game();
   game.getState();
